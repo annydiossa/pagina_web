9d7bd6ca26e6b1c2c854fe07c7b59cfc8a194257
Andres <anny.diossa@udea.edu.co>
2023-08-17 01:00:54 -0500

    Pimer cambio

---
+++ js/main.js
@@ -0,0 +1,40 @@
+let btn, frm, txt;//Variables
+let sound, btn_play, btn_pause;
+
+window.onload = ()=>{
+  
+  sound = new Howl({
+    src: ['https://res.cloudinary.com/da8a3dxxv/video/upload/v1691798536/audio/good-night-160166_n1nciq.mp3']
+  });
+
+  btn_play = document.getElementById("btn_play");
+  btn_pause = document.getElementById("btn_pause");
+  
+  btn = document.getElementById("btn_enviar");
+  btn.addEventListener("click",enviar);//Acción de presion de botón
+   btn_play.addEventListener("click",reproducir);//Acción de presion de botón
+  btn_pause.addEventListener("click",pausar);//Acción de presion de botón
+}
+
+function enviar(event){
+  frm = document.getElementById("formulario");
+    if(frm.checkValidity())
+    {
+      txt = document.getElementById("email");
+      txt.value = "";
+      alert("Gracias por el mensaje. Pronto nos pondremos en contacto contigo");
+      event.preventDefault();  
+    }
+}
+
+function reproducir(event){
+  btn_play.classList.add("ocultar");
+  btn_pause.classList.remove("ocultar");
+  sound.play();
+}
+
+function pausar(event){
+  btn_play.classList.remove("ocultar");
+  btn_pause.classList.add("ocultar");
+  sound.pause();
+}
